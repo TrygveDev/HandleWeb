255fec4f8c2062a2fa53f0b8de3b96859592c05b
Trygve <trygvemf@gmail.com>
2022-12-06 14:30:15 +0100

    Revert "modal rework"
    This reverts commit 8db8387aabd440855e8ea5a774e653b24172b105.

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,39 +1,19 @@
 import "../style/grocery.css";
 import { useState } from "react";
-import { confirmAlert } from 'react-confirm-alert';
-import 'react-confirm-alert/src/react-confirm-alert.css';
 
 function Grocery(props) {
-
-
     const [checked, setChecked] = useState(props.price === 0 ? false : true);
     function handleClick() {
         props.setGroceryIndexState(props.index)
         if (props.deleteMode) {
             props.deleteGrocery(props.index)
         } else {
+            checked ? setChecked(false) : setChecked(true);
             if (!checked) {
                 props.toggleDisplayModal(true);
-                checked ? setChecked(false) : setChecked(true);
             } else if (checked) {
-                confirmAlert({
-                    customUI: ({ onClose }) => {
-                        return (
-                            <div className="confirmModal">
-                                <p>Vil du legge tilbake varen?</p>
-                                <div className="confirmModal-btn">
-                                    <button onClick={onClose}>Nei</button>
-                                    <button onClick={() => {
-                                        checked ? setChecked(false) : setChecked(true);
-                                        props.restoreMoneyFromModal(props.price)
-                                        props.removeGroceryCost(props.index)
-                                        onClose();
-                                    }}>Ja</button>
-                                </div>
-                            </div>
-                        );
-                    }
-                });
+                props.restoreMoneyFromModal(props.price)
+                props.removeGroceryCost(props.index);
             }
         }
     }
