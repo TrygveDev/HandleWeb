d926818c367c6658a741596870037b840deaffa7
Trygve <trygvemf@gmail.com>
2022-12-15 13:17:39 +0100

    +Fix

---
+++ src/components/Desktop.js
@@ -0,0 +1,14 @@
+import '../style/desktop.css';
+function App() {
+    return (
+        <div className="container">
+            <div className="desktop-container">
+                <h1>Hei, sjekk ut vår app på din mobil!</h1>
+                <h3>Åpne denne nettsiden på mobilen din og installer den.</h3>
+            </div>
+        </div>
+
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import Desktop from './components/Desktop';
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { isMobile } from 'react-device-detect';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+if (isMobile) {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+      <Desktop />
+    </React.StrictMode>
+  );
+
+}
+
 
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
