5747ccbe30d3ae9eac3ee9ec47fc5b12512ec271
Trygve <trygvemf@gmail.com>
2022-12-16 12:45:00 +0100

    + Analytics

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import Desktop from './components/Desktop';
 import './index.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { isMobile } from 'react-device-detect';
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyDoCsJ7RUiZOu7vN59YmMhoeJE63YMgJfI",
+  authDomain: "handlewebapp.firebaseapp.com",
+  projectId: "handlewebapp",
+  storageBucket: "handlewebapp.appspot.com",
+  messagingSenderId: "682808408672",
+  appId: "1:682808408672:web:468b39b1255b91f063e105",
+  measurementId: "G-JJG3X9NJE3"
+};
+getAnalytics(initializeApp(firebaseConfig))
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
